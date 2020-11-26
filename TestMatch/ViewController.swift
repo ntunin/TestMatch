722e7b260617d8a083ba38aa81d98675f11b349e
ntunin <nik.tyunin.92@mail.ru>
2020-11-26 18:07:17 +0600

    initial commit

---
+++ TestMatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestMatch
+//
+//  Created by Nick Tyunin on 26.11.2020.
+//  Copyright © 2020 Rosberry. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
